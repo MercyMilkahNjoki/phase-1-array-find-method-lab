f8fcc83e72ea6d7155101288054398e422072d21
MercyMilkahNjokiKahungu <mercy.njoki@student.moringaschool.com>
2022-08-18 21:20:16 +0300

    git add .

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code your solution here
+
+function  superbowlWin(record){
+    const recodResult = record.find(record => record.result === "W");
+    return recodResult? recodResult.year : undefined;
+}
